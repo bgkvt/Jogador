5695b4a14dbf423a1cbaffe51a2928f95a1d024c
bgkvt <victor.mont.goncalves@escola.pr.gov.br>
2024-09-12 22:59:40 -0300

    Add files via upload

---
+++ JS/perguntas.js
@@ -0,0 +1,102 @@
+export const perguntas = [
+    {
+        enunciado: "Primeiro. Quem você acha que deve ser o  melhor jogador do mundo?",
+        alternativas: [
+            {
+                texto: "Cristiano Ronaldo",
+                afirmacao: [
+                    "Após essa resposta ",
+                    
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Ribamar",
+                afirmacao: [
+                    "Tudo bem, Ribamar aceitou sua opção de escolha, ",
+                    "Ribamar não gostou da sua resposta... Ele irá atrás de você e de toda sua família."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Certeza de sua escolha?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "você é sequestrado, e nunca mais vê sua família... adivinhe quem te sequestrou?",
+                    
+                ],
+            },
+            {
+                texto: "Não - Voltar ao início",
+                afirmacao: [
+                    "Você se livrou!!! Evitou o maior desastre de sua vida!!"
+                    
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Certeza de sua escolha?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    " ",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    " ",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Pq você acha que o ribamar tem que ser o melhor do mundo?",
+        alternativas: [
+            {
+                texto: "Tem música, jogou no vascaum da márcia e tem 2 gols em 32 jogos!",
+                afirmacao: [
+                    " ",
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Eu não acho, eu escolhi essa opção por engano...",
+                afirmacao: [
+                    "Só que o presidente do vasco não gostou... e também irá atrás de você e de sua família!!",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Ao escolher ribamar como melhor do mundo, o que você irá fazer",
+        alternativas: [
+            {
+                texto: "1 - Comemorar com ele fazendo um churras na casa dele",
+                afirmacao: [
+                    " "
+                ],
+                proxima: 5,
+            },
+    
+        ]
+    },
+    {
+        enunciado: " Ao chegar na casa dele, você decide chamar ele pra jogar no mengudo!!",
+        alternativas: [
+            {
+                texto: " Clique se você acha que seria uma boa proprosta!!",
+                afirmacao: [
+                    " Assim ,terminando essa longa história!!"
+                ],
+            }
+        ]
+    },
+];
